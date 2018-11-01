064a59049c311bcf79255b4abeabfd51a4a48596
rllin-fathom <randall@fathomhealth.co>
2018-11-01 19:07:15 +0000

    comment out gym -n

--- tensor2tensor/models/__init__.py
+++ tensor2tensor/models/__init__.py
@@ -48,7 +48,7 @@ from tensor2tensor.models.research import gene_expression
 from tensor2tensor.models.research import glow
 from tensor2tensor.models.research import lm_experiments
 from tensor2tensor.models.research import moe_experiments
-from tensor2tensor.models.research import rl
+#from tensor2tensor.models.research import rl
 from tensor2tensor.models.research import similarity_transformer
 from tensor2tensor.models.research import super_lm
 from tensor2tensor.models.research import transformer_moe

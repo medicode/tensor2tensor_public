0e5823e79afe2929872c0ea0c4395ba9d1afb26b
rllin-fathom <randall@fathomhealth.co>
2018-11-29 21:13:26 +0000

    reassign self._opt

--- tensor2tensor/utils/optimize.py
+++ tensor2tensor/utils/optimize.py
@@ -119,7 +119,7 @@ class ConditionalOptimizer(tf.train.Optimizer):
         decr_every_n_nan_or_inf=2,
         incr_ratio=2,
         decr_ratio=0.8)
-    loss_scale_optimizer = tf.contrib.mixed_precision.LossScaleOptimizer(
+    self._opt = tf.contrib.mixed_precision.LossScaleOptimizer(
         self._opt, loss_scale_manager)
 
   def compute_gradients(self, loss, var_list=None, **kwargs):  # pylint: disable=arguments-differ

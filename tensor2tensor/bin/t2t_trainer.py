c1a95dce6df301ac94cd84c846ec846f9941e93f
rllin-fathom <randall@fathomhealth.co>
2018-12-04 00:51:49 +0000

    only step 200

--- tensor2tensor/bin/t2t_trainer.py
+++ tensor2tensor/bin/t2t_trainer.py
@@ -293,9 +293,9 @@ def profile_context():
   if True:
     with tf.contrib.tfprof.ProfileContext(
         #"t2tprof", trace_steps=range(100), dump_steps=range(100)) as pctx:
-        FLAGS.output_dir + "t2tprof", trace_steps=range(200, 202), dump_steps=range(200, 202)) as pctx:
+        FLAGS.output_dir + "t2tprof", trace_steps=range(200, 201), dump_steps=range(200, 201)) as pctx:
       opts = tf.profiler.ProfileOptionBuilder.time_and_memory()
-      pctx.add_auto_profiling("op", opts, range(200, 202))
+      pctx.add_auto_profiling("op", opts, range(200, 201))
       yield
   else:
     yield

44b45ed6804560f866fe95b7bd7464524a9f7959
rllin-fathom <randall@fathomhealth.co>
2019-01-08 00:11:11 +0000

    another cast like

--- tensor2tensor/models/research/universal_transformer_util.py
+++ tensor2tensor/models/research/universal_transformer_util.py
@@ -1918,7 +1918,7 @@ def add_step_timing_signal(x, step, hparams):
         channels, step, num_steps)
 
   if hparams.add_or_concat_timing_signal == "add":
-    x_with_timing = x + signal
+    x_with_timing = x + common_layers.cast_like(signal, x)
 
   elif hparams.add_or_concat_timing_signal == "concat":
     batch_size = common_layers.shape_list(x)[0]

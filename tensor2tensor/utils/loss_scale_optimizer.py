29cd5865d9ea37255da88907798c656d32576ce5
etragas <etragas@gmail.com>
2019-01-22 11:26:12 -0500

    make no nop again

--- tensor2tensor/utils/loss_scale_optimizer.py
+++ tensor2tensor/utils/loss_scale_optimizer.py
@@ -156,7 +156,7 @@ class LossScaleOptimizer(optimizer.Optimizer):
       return self._opt.apply_gradients(grads_and_vars, global_step, name)
     print("True apply grad fn", true_apply_gradients_fn())
     update_vars = control_flow_ops.cond(
-        is_overall_finite, true_apply_gradients_fn, lambda: tf.zeros([1], tf.int64))
+        is_overall_finite, true_apply_gradients_fn, gen_control_flow_ops.no_op)#lambda: tf.zeros([1], tf.int64))
     # Potentially adjust gradient scale in case of finite gradients.
     return control_flow_ops.group(
         update_vars,

74acc768bf230e6221037d2155c22743b42b3e34
etragas-fathom <elias@fathomhealth.co>
2019-02-22 16:18:10 -0500

    tackle trainer

--- tensor2tensor/utils/loss_scale_manager.py
+++ tensor2tensor/utils/loss_scale_manager.py
@@ -67,8 +67,6 @@ class FathomDistributedExponentialUpdateLossScaleManager(
   def update_loss_scale(self, finite_grads):
     """Updates loss scale based on if gradients are finite in current step."""
     #TODO: Fix
-    pass
-    return
     next_step = self._num_good_steps + 1
     # self._loss_scale = tf.cond(finite_grads, )
     def update_if_finite_grads():

024b9f363803aca935533b66d8bd37aafc78af11
etragas-fathom <elias@fathomhealth.co>
2019-04-18 18:13:49 -0400

    drop change

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -39,7 +39,6 @@ from tensorflow.python import debug
 
 # Fathom imports
 from fathomt2t.problems.fprecord_text_problem import FPRecordTextProblem
-from fathomt2t.monitors import FathomValidationMonitor
 
 
 def next_checkpoint(model_dir, timeout_mins=120):
@@ -353,7 +352,7 @@ def create_hooks(use_tfdbg=False,
     assert FLAGS.schedule != 'continuous_train_and_eval'
     
     train_hooks.append(
-        FathomValidationMonitor(restart_after_eval=FLAGS.restart_after_eval,
+        tf.contrib.learn.monitors.ValidationMonitor(
             hooks=eval_hooks, **validation_monitor_kwargs))
 
   if use_early_stopping:

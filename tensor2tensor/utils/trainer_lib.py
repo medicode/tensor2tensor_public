79ee6b06feafd15c766a04c520b7367791b8f890
Parth Doshi <parth@fathomhealth.co>
2020-05-21 12:36:33 -0700

    better resolve tpu names

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -210,6 +210,10 @@ def create_run_config(model_name,
         if project in PROJECT_ZONE:
             zone = PROJECT_ZONE[project]
 
+        if 'KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS' in os.environ:
+            cloud_tpu_meta = os.environ['KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS']
+            zone, cloud_tpu_name = cloud_tpu_meta.split('/')
+
         tf.logging.info(f'Cloud TPU Name: {cloud_tpu_name}\tProject: {project}'
                         f'\tZone: {zone}')
 

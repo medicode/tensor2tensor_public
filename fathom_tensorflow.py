c7d0c534f398f8851300bbdac8d373d2d7316694
branko-fathom <branko@fathomhealth.co>
2023-06-07 09:22:02 -0400

    new wrapper for tensorflow

---
+++ fathom_tensorflow.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+from tensorflow import *
+compat.v1.disable_v2_behavior()
---
+++ test.py
@@ -0,0 +1,3 @@
+import fathom_tensorflow as tf
+flags = tf.compat.v1.flags
+print(tf.executing_eagerly())
